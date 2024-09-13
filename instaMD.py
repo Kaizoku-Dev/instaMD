76b1332cc6a32b5291ebb5ab1ee674ac887c6332
Kaizoku-Dev <kaizokudev1@gmail.com>
2024-09-13 21:21:44 +0530

    Add files via upload

--- instaMD.py
+++ instaMD.py
@@ -1,16 +1,13 @@
-# print("\033[2J\033[1;1H")
 import instaloader
 import re
 
 def get_shortcode_from_url(url):
-    # Regular expression to match the shortcode in the URL
     match = re.search(r'/(p|reel|tv)/([A-Za-z0-9_-]+)/', url)
     if match:
         return match.group(2)
     return None
 
 def download_instagram_post(url):
-    # Create an instance of Instaloader
     L = instaloader.Instaloader(
     download_videos=True,
     download_video_thumbnails=False,
@@ -19,13 +16,11 @@ def download_instagram_post(url):
     post_metadata_txt_pattern=''
     )
 
-    # Extract the shortcode from the URL
-    #
+
     shortcode = get_shortcode_from_url(url)
     
     if shortcode:
         try:
-            # Download the post using its shortcode
             post = instaloader.Post.from_shortcode(L.context, shortcode)
             L.download_post(post, target="downloads")
             print("Post Downloaded")
